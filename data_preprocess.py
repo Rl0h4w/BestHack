69f358be7ef0dab3390d85da3a1c3ad03a600700
rl0h4w <rl0h4w@gmail.com>
2025-03-02 02:28:25 +0300

    start

--- data_preprocess.py
+++ data_preprocess.py
@@ -2,10 +2,23 @@ import os
 import cv2
 from collections import deque
 import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import (
+    Input,
+    Conv3D,
+    MaxPooling3D,
+    BatchNormalization,
+    Reshape,
+    TimeDistributed,
+    Dense,
+    Dropout,
+    LSTM,
+)
 
 
 class Dataset:
-    def __init__(self, name, dir, size_of_window=30):
+    def __init__(self, name, dir, size_of_window=60):
         self.name = name
         self.dir = dir
         self.size_of_window = size_of_window
@@ -28,7 +41,7 @@ class Dataset:
             frame_window = deque(maxlen=self.size_of_window)
             target_window = deque(maxlen=self.size_of_window)
             frame_count = 0
-
+            print(num_dir)
             while True:
                 ret, frame = cap.read()
                 if not ret:
@@ -40,4 +53,65 @@ class Dataset:
                 frame_count += 1
 
                 if frame_count >= self.size_of_window:
-                    yield np.array(frame_window), np.array(target_window)
+                    yield np.array(frame_window) / 255, np.array(target_window)
+
+
+def get_model(input_shape=(60, 240, 320, 3)):
+    inputs = Input(shape=input_shape)
+
+    x = Conv3D(32, kernel_size=(3, 3, 3), activation="relu", padding="same")(inputs)
+    x = MaxPooling3D(pool_size=(1, 2, 2), padding="same")(x)
+    x = BatchNormalization()(x)
+
+    x = Conv3D(64, kernel_size=(3, 3, 3), activation="relu", padding="same")(x)
+    x = MaxPooling3D(pool_size=(1, 2, 2), padding="same")(x)
+    x = BatchNormalization()(x)
+
+    x = Conv3D(128, kernel_size=(3, 3, 3), activation="relu", padding="same")(x)
+    x = MaxPooling3D(pool_size=(1, 2, 2), padding="same")(x)
+    x = BatchNormalization()(x)
+
+    time_steps = input_shape[0]
+    x = Reshape((time_steps, -1))(x)
+
+    x = LSTM(64, return_sequences=True)(x)
+    x = LSTM(32, return_sequences=True)(x)
+    x = TimeDistributed(Dense(256, activation="relu"))(x)
+    x = TimeDistributed(Dropout(0.5))(x)
+    x = TimeDistributed(Dense(1, activation="linear"))(x)
+
+    outputs = Reshape((time_steps,))(x)
+
+    model = Model(inputs=inputs, outputs=outputs)
+    model.compile(
+        optimizer="adam",
+        loss="mse",
+        metrics=["mae", tf.keras.metrics.RootMeanSquaredError(name="rmse")],
+    )
+
+    return model
+
+
+strategy = tf.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1"])
+with strategy.scope():
+    model = get_model(input_shape=(60, 240, 320, 3))
+
+train_ds = Dataset("train", "/kaggle/input/sxuprl/train/train")
+test_ds = Dataset("test", "/kaggle/input/sxuprl/test/test")
+
+
+def create_tf_dataset(dataset):
+    return tf.data.Dataset.from_generator(
+        lambda: dataset,
+        output_signature=(
+            tf.TensorSpec(shape=(60, 240, 320, 3), dtype=tf.float32),
+            tf.TensorSpec(shape=(60,), dtype=tf.float32),
+        ),
+    ).batch(6)
+
+
+train_dataset = create_tf_dataset(train_ds)
+test_dataset = create_tf_dataset(test_ds)
+
+model.fit(train_dataset, validation_data=test_dataset)
+model.save("model.h5")
